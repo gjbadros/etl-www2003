274dc6c8997f42c359d7a43eddd9c366de2c2aab
gjb <>
2003-03-26 19:06:30 +0000

    Updated for new distribution of files, new names.

--- etl-www2003-poster/make-submission.sh
+++ etl-www2003-poster/make-submission.sh
@@ -1,5 +1,4 @@
 #! /bin/bash
-cp ../etl-www2003-poster.pdf .
-rm etl-www2003-poster-final-submission.zip
-zip etl-www2003-poster-final-submission.zip *.*html *.png *.css README.txt etl-www2003-poster.pdf
-
+cp ../etl-www2003-poster.pdf p310-badros.pdf
+rm p310-badros-etl-www2003-poster-final-submission.zip
+zip p310-badros-etl-www2003-poster-final-submission.zip p310-badros.{html,pdf} *.css README.txt
