4c604cb4f21200ddc498e293460c1a04dbb689c1
gjb <>
2003-03-26 19:15:35 +0000

    Do not copy the pdf from .. to here -- that pdf has page numbers, and p310-badros.pdf is independently version-controlled now.

--- etl-www2003-poster/make-submission.sh
+++ etl-www2003-poster/make-submission.sh
@@ -1,4 +1,3 @@
 #! /bin/bash
-cp ../etl-www2003-poster.pdf p310-badros.pdf
 rm p310-badros-etl-www2003-poster-final-submission.zip
 zip p310-badros-etl-www2003-poster-final-submission.zip p310-badros.{html,pdf} *.css README.txt
